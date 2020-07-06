59217a27e0d7b4b5c60c61ac0cefe6e20547d875
Win Htet Aung <winhtetaung@192.168.1.5>
2020-07-07 00:16:43 +0630

    third commit

---
+++ database/seeds/MenuTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MenuTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = factory(\App\Models\Category::class, 5)->create();
+        $categories->each(function ($category){
+            factory(App\Models\Menu::class ,3)->create([
+                'category_id' => $category->id,
+            ]);
+        });
+    }
+}
---
+++ resources/js/modules/menu/MenuContainer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="wrapper menu__container">
+        <p> THIS IS MENU CHECK</p>
+    </div>
+</template>
+
+<script>
+    export default
+    {
+        props: ['items']
+    }
+</script>
